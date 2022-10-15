34710682b89578a435f08200887f17534f9073de
Alice-Caronna <ali.caronna@gmail.com>
2022-10-15 14:27:16 +0200

    aside color change on scroll

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let aside = document.querySelector("aside");
+let asideStyle = window.getComputedStyle(aside);
+let asideBgColor = asideStyle.getPropertyValue("background-color")
+// aside.style.setProperty("background-color", "black");
+console.log(asideBgColor);
+
+
+window.addEventListener("scroll", function(){
+    console.log(window.scrollY)
+    if(this.window.scrollY > 400){
+        aside.style.setProperty("background-color", "white")
+    }else{
+        aside.style.setProperty("background-color", asideBgColor)
+    }
+})
