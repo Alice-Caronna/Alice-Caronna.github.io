dd8843e303d229b31a71bdf3cbb8922060a0b322
Alice-Caronna <ali.caronna@gmail.com>
2022-10-15 16:18:33 +0200

    only on monile

--- js/script.js
+++ js/script.js
@@ -3,13 +3,10 @@ let asideStyle = window.getComputedStyle(aside);
 let asideBgColor = asideStyle.getPropertyValue("background-color")
 let isMobile = navigator.userAgentData.mobile;
 
-
-console.log(asideBgColor);
 console.log(isMobile)
 
 
 window.addEventListener("scroll", function(){
-    // console.log(window.scrollY)
     if(isMobile){
         if(this.window.scrollY > 480 && this.window.scrollY < 1450){
             aside.style.setProperty("background-color", "white")
